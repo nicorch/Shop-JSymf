114d2127b8a770302f0aac8bef6f18cf82f7b2a0
NicolasRoche <nicolas8roche@gmail.com>
2021-04-05 00:56:07 +0200

    Update config and migrations

---
+++ migrations/Version20210403223804.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210403223804 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE option_product (option_id INT NOT NULL, product_id INT NOT NULL, INDEX IDX_CBBE13D8A7C41D6F (option_id), INDEX IDX_CBBE13D84584665A (product_id), PRIMARY KEY(option_id, product_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE option_product ADD CONSTRAINT FK_CBBE13D8A7C41D6F FOREIGN KEY (option_id) REFERENCES `option` (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE option_product ADD CONSTRAINT FK_CBBE13D84584665A FOREIGN KEY (product_id) REFERENCES product (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE `option` DROP FOREIGN KEY FK_5A8600B04584665A');
+        $this->addSql('DROP INDEX IDX_5A8600B04584665A ON `option`');
+        $this->addSql('ALTER TABLE `option` DROP product_id');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE option_product');
+        $this->addSql('ALTER TABLE `option` ADD product_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE `option` ADD CONSTRAINT FK_5A8600B04584665A FOREIGN KEY (product_id) REFERENCES product (id)');
+        $this->addSql('CREATE INDEX IDX_5A8600B04584665A ON `option` (product_id)');
+    }
+}
